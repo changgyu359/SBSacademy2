28bb70b5d185ce677a3075079aed92cc618f7e85
changgyu359 <gchl8548@gmail.com>
2025-08-20 19:44:35 +0900

    (2025.8.20) 열거형

--- Program/Program.c
+++ Program/Program.c
@@ -26,25 +26,57 @@ int main()
 	// 77 Right
 	// 80 Down
 	
-
+	int  x = 0;
+	int  y = 0;
+	move(x, y);
+	printf("★");
 	
-
+	
 	while (1)
 	{
-		char x = _getch();
-		switch (x)
+		move(20, 20);
+		printf("＠<-포탈");
+		char dot = _getch();
+		if (dot == -32)
 		{
-		case UP: printf("UP\n");
+			dot = _getch();
+		}
+		switch (dot)
+		{
+		//case -32:
+		//	break; 그냥 내가 했던 방식
+		case UP: y-=1;
+			if (y < 0)
+			{
+				y = 0;
+			}
 			break;
-		case LEFT: printf("LEFT\n");
+		case LEFT: x-=2;
+			if (x < 0)
+			{
+				x = 0;
+			}
 			break;
-		case RIGHT: printf("RIGHT\n");
+		case RIGHT: x+=2;
 			break;
-		case DOWN: printf("DOWN\n");
+		case DOWN: y+=1;
+			break;
+		default: printf("EXCEPTION\n");
 			break;
 		}
+		system("cls");
+		if (x == 20 & y == 20)
+		{
+			x = 0;
+			y = 0;
+		}
+		move(x, y);
+		printf("★");
 	}
 
+	// w,a,s,d->알파벳은 이미 다 값이 정해져있다 근데 화살표들은?
+	// 위,아래,왼쪽,오른쪽은 두번의 입력으로 처리된다. 처음은 0or-32가 반환, 2번째에서 실제 키를 받음
+	// 처음에 이상값을 줌으로서 입력한 키가 특수한 값이라는걸 알려줌
 	
 
 	return 0;
